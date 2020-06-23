4dfe99b486f3bca91add80eafc5a0722402e7f5c
Diane Cochran <dianeofthemoon@gmail.com>
2020-06-23 14:06:43 -0700

    Update to pull in variables

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-apt-get install -y neovim
+sudo apt-get install -y neovim
+sudo chsh -s `which zsh` `whoami`
+
+if [ -n "$SSH_KEY" ]; then
+    mkdir ~/.ssh
+    echo $SSH_KEY > ~/.ssh/id_ed25519
+    echo $SSH_KEY_PUB > ~/.ssh/id_ed25519.pub
+fi
