7833b46fb6f8e7900f88e55d3e81e5772da4d5a2
Diane Cochran <dianeofthemoon@gmail.com>
2020-06-23 15:06:46 -0700

    updated neovim install script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get install -y software-properties-common
+sudo apt-get update
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip neovim
 sudo chsh -s `which zsh` `whoami`
