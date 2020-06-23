0105fe346beb97080d9c7a4599358bda1625ffa3
Diane Cochran <dianeofthemoon@gmail.com>
2020-06-23 14:53:40 -0700

    install a recent version of neovim

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-sudo apt-get install -y neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip neovim
 sudo chsh -s `which zsh` `whoami`
 
 if [ -n "$SSH_KEY" ]; then
+    echo "Installing keys"
     mkdir ~/.ssh
     echo $SSH_KEY > ~/.ssh/id_ed25519
     echo $SSH_KEY_PUB > ~/.ssh/id_ed25519.pub
