a3121c009da94fd785aa757dd12d1171c7903c85
Diane Cochran <dianeofthemoon@gmail.com>
2020-06-26 15:03:56 -0700

    added xdg specs

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #/bin/sh
 sudo apt-get install -y zsh
 
+sudo chsh -s `which zsh` `whoami`
+echo 'export XDG_CONFIG_HOME="$HOME/.config"' > /etc/zsh/zshenv
+echo 'export XDG_DATA_HOME="$HOME/.local/share"' > /etc/zsh/zshenv
+echo 'export XDG_CACHE_HOME="$HOME/.cache"' > /etc/zsh/zshenv
+echo 'export ZDOTDIR="$XDG_CONFIG_HOME/zsh"' > /etc/zsh/zshenv
+
 git config --global user.email "dianeofthemoon@gmail.com"
 git config --global user.name "Diane Cochran"
 
-sudo chsh -s `which zsh` `whoami`
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 if [ -n "$SSH_KEY" ]; then
     echo "Installing keys"
